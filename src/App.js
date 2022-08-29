fa2c78371f5b9567b1d0b486427ecf15b8d4c59a
oscar anillo <oscarani14@gmail.com>
2022-08-29 12:26:17 -0500

    add image and text components with their styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import ImageComponent from './components/image-component';
+import NavBarComponent from './components/nav-component';
+import TextComponent from './components/text-component';
 
 import './App.css';
-import NavBarComponent from './components/nav-component';
 
 function App() {
   return (
     <div className="App">
       <NavBarComponent />
+      <div>
+        <ImageComponent />
+        <TextComponent />
+      </div>
     </div>
   );
 }
